ddd2490ff0081e4ada0118db6ae9636dc42eede2
FerPoxa <113585786+FerPoxa@users.noreply.github.com>
2024-01-15 13:15:09 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import platform
+import re
+def print_interfaces():
+    if platform.system() == "Windows":
+        os.system('powershell Get-NetAdapter -Name *')
+    elif platform.system() == "Linux":
+        os.system("ip a | grep -E '[0-9]+:\s(\w+):'")
+    select = input ('Enter the interface name: ')
+    return select
+def change_mac():   
+    if platform.system() == "Windows":
+        interface = print_interfaces()
+        mac = input("Enter your spoofed mac")
+        os.system('powershell Set-NetAdapter -Name "'+interface+'" -MacAddress "'+mac+'"')
+    elif platform.system() == "Linux":
+        os.system("sudo ip link set dev "+print_interfaces+" address "+"mac")
+    print ("Mac changed to: "+mac)
